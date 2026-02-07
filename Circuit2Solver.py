bc18d8bd338e15a74d8aed7162c55b057f477188
Mina <mina.papahnzadeh@joinliora.com>
2026-02-06 22:26:49 -0600

    Implement PR review suggestions

--- Circuit2Solver.py
+++ Circuit2Solver.py
@@ -1,8 +1,6 @@
-
 import numpy as np
 
 from scipy.optimize import fsolve
-
 
 # Known resistances (ohms)
 R3 = 330.0
@@ -10,13 +8,14 @@ R4 = 165.0
 
 # Measured voltages (VOLTS)
 # =========================
-V_unload = 10 # voltage across R3 alone
-V_24     = 12 # voltage with R2 || R4
-V_load   = 30 # voltage with R2 || R3 || R4
+V_unload = 10  # voltage across R3 alone
+V_24 = 12  # voltage with R2 || R4
+V_load = 30  # voltage with R2 || R3 || R4
 
 # Voltage ratios
 A = V_24 / V_unload
 B = V_load / V_unload
+
 
 # Define equations
 def equations(vars):
@@ -24,15 +23,16 @@ def equations(vars):
 
     # Parallel resistances
     R24 = (R2 * R4) / (R2 + R4)
-    Req = 1 / (1/R2 + 1/R3 + 1/R4)
+    Req = 1 / (1 / R2 + 1 / R3 + 1 / R4)
 
     eq1 = A - (R24 * (R3 + R1)) / (R3 * (R24 + R1))
     eq2 = B - (Req * (R3 + R1)) / (R3 * (Req + R1))
 
     return [eq1, eq2]
 
+
 # Initial guess
-initial_guess = [500, 10]  
+initial_guess = [500, 10]
 
 # Solve
 R2_sol, R1_sol = fsolve(equations, initial_guess)
